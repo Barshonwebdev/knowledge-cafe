06da74cda46d65d922b3e300109d8be7e1808f52
Barshon <barshonweb@gmail.com>
2023-09-10 12:28:38 +0600

    header created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className='header'>
+            <h2>Knowledge Cafe</h2>
+            <img src="../../../public/student-5.png" alt="" />
+        </div>
+    );
+};
+
+export default Header;
