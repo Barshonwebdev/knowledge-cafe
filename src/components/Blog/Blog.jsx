f898f1c7f2cc7d4084e9f70b0965cc0311ed4d0d
Barshon <barshonweb@gmail.com>
2023-09-11 09:30:06 +0600

    fontawesome bookmark icon added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import './Blog.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark } from "@fortawesome/free-solid-svg-icons";
 const Blog = (props) => {
-    const {blogTitle}=props.blog;
+    const {blogTitle,blogCover}=props.blog;
     return (
-        <div>
+        <div className='each-blog'>
+            <img src={blogCover} alt="" />
+            <div className='blog-infos'>
+                <div className='blog-author'>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+                <div className='blog-read'>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
             <h3>{blogTitle}</h3>
-        </div>
+            <p></p>
+            <a href='#'>Mark as read</a>
+            <FontAwesomeIcon className='icon' icon={faBookmark}></FontAwesomeIcon>
+        </div> 
     );
 };
 
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -13,7 +13,7 @@ const Blogs = () => {
     return (
         <div className='blogs-container'>
             {
-                blogs.map(blog=><Blog key={blog.id} blog={blog}></Blog>)
+                blogs.map(blog=><Blog key={blog.id} blog={blog}></Blog>) 
             }
         </div>
     );
